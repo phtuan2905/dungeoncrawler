7f9b04bce4d661db3f125d9b47f49fb5cb998e50
phtuan2905 <tuannguyen20905@gmail.com>
2025-01-23 07:39:10 +0700

    v.0.1.5

--- Assets/Scripts/ProceduralDungeonGenerate.cs
+++ Assets/Scripts/ProceduralDungeonGenerate.cs
@@ -9,62 +9,148 @@ public class ProceduralDungeonGenerate : MonoBehaviour
     [SerializeField] private Tilemap groundTilemap;
     [SerializeField] private Tile groundTile;
 
-    [SerializeField] private List<Vector3Int> ways;
-    [SerializeField] private List<Vector3Int> currentWays;
-    [SerializeField] private Vector3Int roomCenter;
-    [SerializeField] private Vector3Int roomSize;
-    [SerializeField] private int wayIndex;
+    public List<Vector3Int> directions;
 
     [SerializeField] private int roomTotal;
     [SerializeField] private List<Room> rooms;
     [SerializeField] private int roomIndex;
+    [SerializeField] private int maxRoomNumber;
+
+    [Header("Dungeon Size")]
+    [SerializeField] private int minX;
+    [SerializeField] private int maxX;
+    [SerializeField] private int minY;
+    [SerializeField] private int maxY;
+
+    [System.Serializable] class Room
+    {
+        public Room()
+        {
+            roomDirections = new List<Vector3Int>();
+        }
+
+        public Vector3Int roomCenter;
+        public Vector3Int roomSize;
+        public List<Vector3Int> roomDirections;
+    }
 
     private void Awake()
-    {         
-        ways.Add(Vector3Int.up);
-        ways.Add(Vector3Int.right);
-        ways.Add(Vector3Int.down);
-        ways.Add(Vector3Int.left);
-        roomCenter = (Vector3Int.zero + new Vector3Int(5, 5, 0)) / 2;
-        roomSize = new Vector3Int(5, 5, 0);
+    {   
+        directions.Add(Vector3Int.up);
+        directions.Add(Vector3Int.right);
+        directions.Add(Vector3Int.down);
+        directions.Add(Vector3Int.left);
+        isNotGenerate = false;
+        GenerateDungeon();
+    }
+
+    bool isNotGenerate;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isNotGenerate)
+        {
+            groundTilemap.ClearAllTiles();
+            isNotGenerate = false;
+            GenerateDungeon();
+        }
+    }
+
+    void GenerateDungeon()
+    {
+        Vector3Int roomCenter = (Vector3Int.zero + new Vector3Int(5, 5, 0)) / 2;
+        Vector3Int roomSize = new Vector3Int(5, 5, 0);
         DrawRoom(Vector3Int.zero, roomSize);
 
-        Room baseRoom = Instantiate(new Room());
+        rooms = new List<Room>();
+
+        Room baseRoom = new Room();
         baseRoom.roomCenter = roomCenter;
         baseRoom.roomSize = roomSize;
-        baseRoom.roomDirections = ways;
+        baseRoom.roomDirections = new List<Vector3Int>(directions);
         rooms.Add(baseRoom);
+
+        maxX = 5;
+        maxY = 5;
+        minX = 0;
+        minY = 0;
 
         roomIndex = 0;
         while (roomTotal > 0)
         {
-            int roomNumber = Random.Range(1, 3);
+            int roomNumber = Random.Range(1, maxRoomNumber + 1);
+            //Debug.Log(roomNumber);
             for (int i = 0; i < roomNumber; i++)
             {
-                GenerateDungeon(rooms[roomIndex]);
-                roomIndex++;
+                GenerateRoom(rooms[roomIndex]);
                 roomTotal--;
-                if (roomTotal == 0) 
+                if (roomTotal == 0)
                 {
                     break;
                 }
             }
+            roomIndex++;
         }
+        isNotGenerate = true;
     }
 
-    void GenerateDungeon(Room parentRoom)
+    void GenerateRoom(Room parentRoom)
     {
+        //Debug.Log(parentRoom.roomCenter);
         int directionIndex = Random.Range(0, parentRoom.roomDirections.Count);
-        Vector3Int wayStartPoint = parentRoom.roomCenter + parentRoom.roomDirections[directionIndex] * parentRoom.roomSize / 2;
-        DrawCorridor(wayStartPoint, parentRoom.roomDirections[directionIndex], 3);
-        Vector3Int wayEndPoint = parentRoom.roomCenter + parentRoom.roomDirections[directionIndex] * parentRoom.roomSize / 2 + parentRoom.roomDirections[directionIndex] + parentRoom.roomDirections[directionIndex] * 3;
+        Vector3Int direction = parentRoom.roomDirections[directionIndex];
+        Vector3Int wayStartPoint = parentRoom.roomCenter + direction * parentRoom.roomSize / 2;
         Room newRoom = new Room();
-        newRoom.roomSize = new Vector3Int(5, 5, 0);
-        newRoom.roomCenter = wayEndPoint + parentRoom.roomDirections[directionIndex] * newRoom.roomSize / 2;
-        newRoom.parentRoom = parentRoom;
+        int roomSizeX = Random.Range(4, 14);
+        if (roomSizeX % 2 == 0) roomSizeX++;
+        int roomSizeY = Random.Range(4, 14);
+        if (roomSizeY % 2 == 0) roomSizeY++;
+        newRoom.roomSize = new Vector3Int(roomSizeX, roomSizeY, 0);
+        int bonusLenght = 0;
+        if (direction == Vector3Int.up)
+        {
+            bonusLenght = maxY - wayStartPoint.y;
+        }
+        else if (direction == Vector3Int.down)
+        {
+            bonusLenght = wayStartPoint.y - minY;
+        }
+        else if (direction == Vector3Int.right)
+        {
+            bonusLenght = maxX - wayStartPoint.x;
+        }
+        else if (direction == Vector3Int.left)
+        {
+            bonusLenght = wayStartPoint.x - minX;
+        }
+        Vector3Int wayEndPoint = parentRoom.roomCenter + direction * parentRoom.roomSize / 2 + direction + direction * (3 + bonusLenght);
+        newRoom.roomCenter = wayEndPoint + direction * newRoom.roomSize / 2;
+        newRoom.roomDirections = new List<Vector3Int>(directions);
+        newRoom.roomDirections.Remove(direction * -1);
+        Vector3Int roomStartPoint = newRoom.roomCenter - newRoom.roomSize / 2;
+        Vector3Int roomEndPoint = roomStartPoint + newRoom.roomSize - Vector3Int.one;
+
+        DrawCorridor(wayStartPoint, parentRoom.roomDirections[directionIndex], 3 + bonusLenght);
         DrawRoom(newRoom.roomCenter - newRoom.roomSize / 2, newRoom.roomSize);
+
         rooms.Add(newRoom);
-        parentRoom.roomDirections.Remove(parentRoom.roomDirections[directionIndex] * -1);
+        parentRoom.roomDirections.Remove(direction);
+        if (roomStartPoint.x < minX)
+        {
+            minX = roomStartPoint.x;
+        }
+        if (roomStartPoint.y < minY)
+        {
+            minY = roomStartPoint.y;
+        }
+        if (roomEndPoint.x > maxX)
+        {
+            maxX = roomEndPoint.x;
+        }
+        if (roomEndPoint.y > maxY)
+        {
+            maxY = roomEndPoint.y;
+        }
     }
 
     void DrawRoom(Vector3Int startPoint, Vector3Int roomSize)
@@ -85,15 +171,4 @@ public class ProceduralDungeonGenerate : MonoBehaviour
             groundTilemap.SetTile(startPoint + (direction * i), groundTile);
         }
     }
-}
-
-class Room
-{
-    public Room() { }
-
-    public Vector3Int roomCenter;
-    public Vector3Int roomSize;
-    public Room parentRoom;
-    public List<Vector3Int> roomDirections;
-
 }
